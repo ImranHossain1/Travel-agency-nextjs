474adef4b6f626aa1f920041e119e99cc449dfa8
Md Imran Hossain <imranhossain1402@gmail.com>
2023-10-17 23:42:38 +0200

    pages created

---
+++ src/components/ui/Navbar/Navbar.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { showSidebarDrawer } from "@/redux/slices/sidebarSlice";
+import { MenuOutlined } from "@ant-design/icons";
+import { Button, Drawer, Layout, Menu, Typography } from "antd";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const { Header, Content } = Layout;
+const { Title } = Typography;
+
+const Navbar = ({
+  items,
+  hasSider,
+}: {
+  items: { key: string; label: string; href: string }[];
+  hasSider?: boolean;
+}) => {
+  const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <Layout className="layout">
+      <Header className="flex items-center">
+        {hasSider && (
+          <Button
+            type="primary"
+            className="lg:hidden"
+            onClick={() => {
+              dispatch(showSidebarDrawer());
+            }}
+          >
+            <MenuOutlined />
+          </Button>
+        )}
+        <Content>
+          <Link href="/">
+            <Title
+              className={`m-0 text-white ${
+                hasSider && "text-center lg:text-left"
+              }`}
+            >
+              Doctors Portal
+            </Title>
+          </Link>
+        </Content>
+        <Menu
+          className="lg:block hidden"
+          disabledOverflow
+          theme="dark"
+          mode="horizontal"
+          selectedKeys={[pathname]}
+        >
+          {items?.map((item) => (
+            <Menu.Item key={item.href}>
+              <Link href={item.href}>{item.label}</Link>
+            </Menu.Item>
+          ))}
+        </Menu>
+
+        <Button type="primary" className="lg:hidden" onClick={showDrawer}>
+          <MenuOutlined />
+        </Button>
+        <Drawer title="Menu" placement="right" onClose={onClose} visible={open}>
+          <Menu
+            theme="light"
+            mode="vertical"
+            selectedKeys={[pathname]}
+            style={{ borderRight: 0 }}
+          >
+            {items?.map((item) => (
+              <Menu.Item key={item.href}>
+                <Link href={item.href}>{item.label}</Link>
+              </Menu.Item>
+            ))}
+          </Menu>
+        </Drawer>
+      </Header>
+    </Layout>
+  );
+};
+
+export default Navbar;
